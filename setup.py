aaa581637199a012864ba7e7bd691a9c256460ca
Philipp Flotho <PhilippFlotho@gmail.com>
2025-12-18 10:14:55 +0900

    added pre-commits, updated the toml to use dynamic versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+"""
+Minimal setup.py for backwards compatibility and editable installs.
+All configuration is in pyproject.toml.
+"""
 
-setup(
-    name="yolov5-face",
-    version="0.0.1-alpha.1",
-    description="Real-time face detection using YOLOv5 - Forked Version",
-    license="GPL-3.0",
-    packages=find_packages(),
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-    ],
-)
+from setuptools import setup
+
+# This enables:
+# - pip install .
+# - pip install -e .
+# - python setup.py develop (deprecated but still used)
+#
+# All actual configuration comes from pyproject.toml
+setup()
